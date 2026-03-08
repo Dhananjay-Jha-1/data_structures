e9398cb7008223132628048fec821b4849e5fd03
Dhananjay-Jha-1 <dhananjayjokes@gmail.com>
2026-03-08 20:10:01 +0530

    remove duplicate elements in a linked list

--- linkedList/13_linkedlist.c
+++ linkedList/13_linkedlist.c
@@ -1,8 +1,7 @@
-// to check if the linked list is sorted or not
+// remove duplicate elements from a sorted linked list
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 struct Node
 {
@@ -16,22 +15,24 @@ void input_ll(void);
 
 void create_ll(int A[], int n);
 
-int check_sorted(struct Node* p);
+void remove_duplicates(struct Node* p);
+
+void display_ll(struct Node* p);
 
 int main(void)
 {
     input_ll();
-   
-    if(check_sorted(first) != 0)
-        printf("The linked list is sorted\n");
-    else
-        printf("The linked list is not sorted\n");
+
+    remove_duplicates(first);
+
+    printf("The list with the removed duplicates is :");
+    display_ll(first);
 }
 
 void input_ll(void)
 {
     int n;
-    printf("Enter the number of elements in the list : ");
+    printf("Enter the number of elements : ");
     scanf("%d", &n);
 
     int A[n];
@@ -67,48 +68,33 @@ void create_ll(int A[], int n)
     }
 }
 
-int check_sorted(struct Node* p)
+void remove_duplicates(struct Node* p)
 {
-    int x = INT_MIN;
-    while(p!=NULL)
+    struct Node* q = p->next;
+    while(q!=NULL)
     {
-        if(p->data < x)      
-            return 0;
-        x = p->data;
-        p = p->next;
+        if(q->data != p->data)
+        {
+           p = q;
+           q = q->next;
+        }
+        else
+        {
+            p->next = q->next;
+            free(q);
+            q = p->next;
+        }
     }
-    return 1;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void display_ll(struct Node* p)
+{
+    while(p!=NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
 
 
 
